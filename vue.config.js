500c5cf2de85696279872a8af34538b81a4b0f35
mckrava <mckrava2@gmail.com>
2021-01-07 17:26:08 +0100

    Clean up vue config

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,12 @@
 module.exports = {
-  lintOnSave: false,
+  lintOnSave: true,
   // productionTip: false,
   configureWebpack: {
     resolve: {
-      // .mjs needed for https://github.com/graphql/graphql-js/issues/1272
-      extensions: ['*', '.mjs', '.js', '.vue', '.json', '.gql', '.graphql'],
+      extensions: ['*', '.mjs', '.js', '.vue', '.json'],
     },
     module: {
       rules: [
-        // fixes https://github.com/graphql/graphql-js/issues/1272
         {
           test: /\.mjs$/,
           include: /node_modules/,
