6cfe5df6f7fc0b1184408f28f322578446f28a97
mckrava <mckrava2@gmail.com>
2021-01-07 17:20:28 +0100

    Update polkadot api

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
 module.exports = {
   lintOnSave: false,
   // productionTip: false,
+  configureWebpack: {
+    resolve: {
+      // .mjs needed for https://github.com/graphql/graphql-js/issues/1272
+      extensions: ['*', '.mjs', '.js', '.vue', '.json', '.gql', '.graphql'],
+    },
+    module: {
+      rules: [
+        // fixes https://github.com/graphql/graphql-js/issues/1272
+        {
+          test: /\.mjs$/,
+          include: /node_modules/,
+          type: 'javascript/auto',
+        },
+      ],
+    },
+  },
 };
