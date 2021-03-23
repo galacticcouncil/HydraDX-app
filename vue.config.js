607c355b016924854d34803b9aabe85c216d3679
smupyknight <smupyknight@outlook.com>
2021-03-23 09:28:07 +0100

    Fix wasm import

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   lintOnSave: true,
   chainWebpack: config => config.resolve.symlinks(false),
@@ -13,7 +15,15 @@ module.exports = {
           include: /node_modules/,
           type: 'javascript/auto',
         },
+        {
+          test: /\.js$/,
+          loader: require.resolve('@open-wc/webpack-import-meta-loader'),
+        },
       ],
     },
+    devServer: {
+      contentBase: path.join(__dirname, 'node_modules/hack-hydra-dx-wasm/build/web'),
+      contentBasePublicPath: '/node_modules/hack-hydra-dx-wasm/build/web',
+    }
   },
 };
